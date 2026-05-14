af47708bdac2ae4bb39fa9bbbfafe2859ccf6653
Kakabi Christian <kakabichristian@gmail.com>
2026-05-14 18:32:08 +0100

    feat(header): Mise en place du header de notre site web

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import  './theme.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Si tu as besoin des composants JS de Bootstrap (dropdowns, tooltips, etc.)
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
